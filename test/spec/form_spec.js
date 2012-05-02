cc1ae08fc171be24579f6268d4ab2cf943a56b7c
Tyler Kellen <tyler@sleekcode.net>
2012-05-02 16:09:48 -0500

    support tag and set modifier functions in form options

--- test/spec/form_spec.js
+++ test/spec/form_spec.js
@@ -3,10 +3,10 @@ define(['performer','spec/helpers/schema'], function(Performer, test_schema) {
 
   describe('Performer.Form', function() {
 
-    var form;
+    var schema, blueprint, options, form;
     beforeEach(function() {
-      var schema = new Performer.Schema(test_schema);
-      var blueprint = Performer.Blueprints.html5();
+      schema = new Performer.Schema(test_schema);
+      blueprint = Performer.Blueprints.html5();
       blueprint.replace('address',{
         addr1: { blueprint: 'text' },
         addr2: { blueprint: 'text' },
@@ -14,15 +14,17 @@ define(['performer','spec/helpers/schema'], function(Performer, test_schema) {
         state: { blueprint: 'text' },
         zip: { blueprint: 'text' }
       });
-      var options = {
+      options = {
         pipelines: {
           tag: [Performer.Transformers.Wrap.div],
           set: [Performer.Transformers.Wrap.fieldset]
         },
-        blueprint: blueprint
+        blueprint: blueprint,
+        modifiers: {}
       };
 
       form = new Performer.Form(schema, options);
+      window.test = form;
     });
 
     it('should be defined', function() {
@@ -34,6 +36,16 @@ define(['performer','spec/helpers/schema'], function(Performer, test_schema) {
       expect(test).toThrow(new Error("Cannot create Form without valid Schema and Blueprint."));
     });
 
+    it('should allow a tag modifier to alter a tag\'s data before generation',function(){
+      options.modifiers.tag = function(data, name) {
+        data.attributes.modified = 'yup';
+        return data;
+      };
+      var test = new Performer.Form(schema, options);
+      var result = test.build({node:'name'});
+      expect(result).toEqual('<div><input type="text" modified="yup" id="name" name="name"/></div>');
+    });
+
     it('should be able to build a single form element without running a group pipeline',function(){
       var result = form.build({node:'name'});
       expect(result).toEqual('<div><input type="text" id="name" name="name"/></div>');
@@ -42,6 +54,16 @@ define(['performer','spec/helpers/schema'], function(Performer, test_schema) {
     it('should build a section of form schema without traversing',function() {
       var result = form.build({node:'account'});
       expect(result).toEqual('<fieldset><div><input type="text" placeholder="placeholder text" id="username" name="username"/></div><div><input type="password" id="password" name="password"/></div><div><input type="hidden" name="is_admin" value="false"/><input type="checkbox" id="is_admin" name="is_admin"/></div></fieldset>');
+    });
+
+    it('should allow a set modifier to alter a section\'s data before generation',function() {
+      options.modifiers.set = function(data, name) {
+        delete data.username;
+        return data;
+      };
+      var test = new Performer.Form(schema, options);
+      var result = test.build({node:'account'});
+      expect(result).toEqual('<fieldset><div><input type="password" id="password" name="password"/></div><div><input type="hidden" name="is_admin" value="false"/><input type="checkbox" id="is_admin" name="is_admin"/></div></fieldset>');
     });
 
     it('should allow prefixing IDs during output',function() {
